430cb9eebb0ff730027902689d5c2c4db1f87d9a
vakulenkoannag <vakulenkoannag@mail.ru>
2017-11-15 21:43:18 +0300

    bonus2

---
+++ bonus/bonus2.py
@@ -0,0 +1,5 @@
+celsius = float(input('Введите температуру в градусах Цельсия °C: '))
+fahrenheit = celsius * (9/5) + 32
+kelvin = celsius + 273.15
+print ('Температура в градусах Фаренгейта: ', fahrenheit, '°F')
+print ('Температура в градусах Кельвина: ', kelvin, '°K')
