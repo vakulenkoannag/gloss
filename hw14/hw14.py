5af61f12aa7322163f30b8ae08da31c208b3ef1e
vakulenkoannag <31884491+vakulenkoannag@users.noreply.github.com>
2018-06-04 20:33:00 +0300

    hw14

---
+++ hw14/hw14.py
@@ -0,0 +1,32 @@
+# можно вечно смотреть на три вещи - огонь, воду и как питон распечатывает миллиард списков. поэтому я оставила в файле семь глав.
+
+import re
+
+def open_file():
+    with open('Pride_and_Prejudice_cut.txt', encoding='utf-8') as f:
+        text = f.read()
+    return text
+
+def clean_everything(text):
+    text = text.replace('Mrs.', 'Mrs')
+    text = text.replace('Mr.', 'Mr')
+    phrases = re.split('[.!?…]+', text)
+    clean_phrases = [re.sub('[,:;—–()&“”«»/*\"\'\n]', '', phrase) for phrase in phrases]
+    return clean_phrases
+
+def get_title_words(clean_phrases):
+    for clean_phrase in clean_phrases:
+        words = clean_phrase.split()
+        length = len(words)
+        title_words = []
+        if length > 10:
+            for word in words:
+                if word.istitle():
+                    title_words.append(word)
+            print(title_words)
+
+def final():
+    text = open_file()
+    get_title_words(clean_everything(text))
+
+final()
